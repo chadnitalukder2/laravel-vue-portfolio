8091ed50b9e6feb3f4982922c20f7c64984abe55
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-06-07 00:43:46 +0600

    database seed

--- banckend/database/migrations/2024_05_24_181336_create_headers_table.php
+++ banckend/database/migrations/2024_05_24_181336_create_headers_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('headers', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->default('I AM DESIGNER');
-            $table->string('short_title')->default('Creative Design and Web Solution Design');
-            $table->string('short_desc')->default('Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et.');
+            $table->string('title');
+            $table->string('short_title');
+            $table->text('short_desc')->nullable();
             $table->string('image')->default('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnRba0yhjuus0apDsjQjLkNyV6y4xVsxoyn0IXOOfSqrh0OUQcsPeih9P2S6gR1PbvoEc&usqp=CAU');
             $table->timestamps();
         });
